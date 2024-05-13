e6cfee3eafbf1ce0494b4f9aaff24579b52fe03e
endrewberthold <endrew.berthold@gmail.com>
2024-05-12 21:56:26 -0300

    Update

---
+++ Documentação/src/Comentarios.java
@@ -0,0 +1,17 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //*Comentario de uma linha */
+
+        /* comentarios de
+         * multiplas linhas
+         */
+    }
+    /**
+     * mais utilizado a nivel de documentação
+     * é utilizado fora do bloco, diferente
+     * dos dois anteriores
+     */
+    public void metodo(){
+
+    }
+}
